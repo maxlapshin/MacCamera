94fc140db6be406e9ca3689a9d3922541b716ef2
Alexander Zhouravlev <bofh@mail.ru>
2010-01-26 14:14:48 +0300

    Class for image convertion added. refs #1

---
+++ UYVY422ToYUV420P.h
@@ -0,0 +1,16 @@
+//
+//  UYVY422ToYUV420P.h
+//  MacCamera
+//
+//  Created by sK0T on 26.01.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface UYVY422ToYUV420P : NSObject {
+
+}
+
+@end
---
+++ UYVY422ToYUV420P.m
@@ -0,0 +1,14 @@
+//
+//  UYVY422ToYUV420P.m
+//  MacCamera
+//
+//  Created by sK0T on 26.01.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "UYVY422ToYUV420P.h"
+
+
+@implementation UYVY422ToYUV420P
+
+@end
